fd4e00bcebf087dc135fe7af45531b769d6986d8
lakshmithirupathammapathi <41730938+lakshmithirupathammapathi@users.noreply.github.com>
2018-07-27 11:00:19 +0530

    Create guvi4.java

---
+++ guvi4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	char c='a';
+	if((c>='a'&&c<='z')||(c>='A'&&c<='z'))
+	System.out.println(c+"is an alphabet");
+	else
+	System.out.println(c+"is not an alphabet");
+	}
+
+}
